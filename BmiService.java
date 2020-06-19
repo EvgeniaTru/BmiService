2f93ed2f68c99562298f9ce02cb2fd05c8ce9e1c
Evgeniia Trusheva <evgenia.tru@gmail.com>
2020-06-19 11:51:41 +0400

    Code reformated

--- BmiService.java
+++ BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public float calculate (float height, float weight) {
-        float index = weight/ (height*height);
+    public float calculate(float height, float weight) {
+        float index = weight / (height * height);
         return index;
     }
 }
